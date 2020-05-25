d35813b1f6c1d8ea0bbfc207bec1a2c0b61fd686
Naymov Aleksey <n.a.v.ru@mail.ru>
2020-05-26 00:45:27 +0300

    Конф

--- source/js/mainmenu.js
+++ source/js/mainmenu.js
@@ -4,11 +4,4 @@ var menu = document.querySelector(".main-nav__wrapper");
 btn.addEventListener("click", function(evt) {
     evt.preventDefault();
     menu.classList.toggle("main-nav__menu-close")
-        /*if (!btn.classList.contains("main-nav__toggle--open") {
-                evt.preventDefault();
-                btn.classList.add("main-nav__toggle--open")
-            } else {
-                btn.classList.add("main-nav__toggle--close")
-            }
-        }*/
 });
