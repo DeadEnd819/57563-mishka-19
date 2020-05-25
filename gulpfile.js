472b5502df3be472d5ee01f8993577f01a2ab6cf
Naymov Aleksey <n.a.v.ru@mail.ru>
2020-05-26 02:23:54 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -82,7 +82,7 @@ gulp.task("webp", function() {
 
 gulp.task("server", function() {
     server.init({
-        server: "source/",
+        server: "build/",
         notify: false,
         open: true,
         cors: true,
--- source/js/mainmenu.js
+++ source/js/mainmenu.js
@@ -1,7 +1,12 @@
 var btn = document.querySelector(".main-nav__toggle");
 var menu = document.querySelector(".main-nav__wrapper");
 
+document.addEventListener("DOMContentLoaded", function() {
+    btn.classList.remove(".main-nav__toggle-hidden");
+    btn.classList.add("main-nav__toggle-visible");
+});
+
 btn.addEventListener("click", function(evt) {
     evt.preventDefault();
-    menu.classList.toggle("main-nav__menu-close")
+    menu.classList.toggle("main-nav__menu-close");
 });
